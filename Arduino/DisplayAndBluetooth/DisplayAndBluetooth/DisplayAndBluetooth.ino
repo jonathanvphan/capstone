0f384090ff26eeedba709609171eeccac3fba6dc
Jonathan Phan <jonathanvphan@gmail.com>
2021-11-15 17:53:12 -0500

    Python and Arduino changes, android folder renamed

--- Arduino/DisplayAndBluetooth/DisplayAndBluetooth/DisplayAndBluetooth.ino
+++ Arduino/DisplayAndBluetooth/DisplayAndBluetooth/DisplayAndBluetooth.ino
@@ -4,21 +4,26 @@
 #include <Wire.h>
 #include <SoftwareSerial.h> 
 
-SoftwareSerial MyBlue(0, 1); // RX | TX 
-U8G2_SSD1309_128X64_NONAME0_F_4W_SW_SPI u8g2(U8G2_R0, /* clock=*/ 13, /* data=*/ 11, /* cs=*/ 10, /* dc=*/ 9, /* reset=*/ 8);
+SoftwareSerial MyBlue(16, 17); // RX | TX 
+U8G2_SSD1309_128X64_NONAME0_F_4W_SW_SPI u8g2(U8G2_R1, /* clock=*/ 13, /* data=*/ 11, /* cs=*/ 10, /* dc=*/ 9, /* reset=*/ 8);
 
 // Create a U8g2log object
 U8G2LOG u8g2log;
 
 // assume 4x6 font, define width and height
-#define U8LOG_WIDTH 20
-#define U8LOG_HEIGHT 6
+#define U8LOG_WIDTH1 13
+#define U8LOG_HEIGHT1 16
+#define U8LOG_WIDTH2 10
+#define U8LOG_HEIGHT2 13
+#define U8LOG_WIDTH3 9
+#define U8LOG_HEIGHT3 12
 
 // allocate memory
-uint8_t u8log_buffer[U8LOG_WIDTH*U8LOG_HEIGHT];
-
-int displayWidth = 128;
-int displayHeight = 64;
+uint8_t u8log_buffer1[U8LOG_WIDTH1*U8LOG_HEIGHT1];
+uint8_t u8log_buffer2[U8LOG_WIDTH2*U8LOG_HEIGHT2];
+uint8_t u8log_buffer3[U8LOG_WIDTH3*U8LOG_HEIGHT3];
+int displayWidth = 64;
+int displayHeight = 128;
 unsigned long passedTime;
 unsigned long clearDisplayTime = 5000;
 String incomingString = "";
@@ -29,8 +34,29 @@ void setup(void) {
   MyBlue.begin(9600); 
   Serial.println("Ready to connect\nDefault password is 1234 or 000"); 
   u8g2.begin();
-  u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font
-  u8g2log.begin(u8g2, U8LOG_WIDTH, U8LOG_HEIGHT, u8log_buffer);  // connect to u8g2, assign buffer
+  u8g2.setFont(u8g2_font_profont12_tf); // choose a suitable font
+  u8g2log.begin(u8g2, U8LOG_WIDTH2, U8LOG_HEIGHT2, u8log_buffer2);  // connect to u8g2, assign buffer
+  u8g2log.setLineHeightOffset(0); // set extra space between lines in pixel, this can be negative
+  u8g2log.setRedrawMode(0);   // 0: Update screen with newline, 1: Update screen for every char  
+  passedTime = millis();
+}
+
+void changeFont(int fontSize) {
+  if (fontSize == 1) {
+    u8g2log.print("Font size set to 1");
+    u8g2.setFont(u8g2_font_profont10_tf); // choose a suitable font
+    u8g2log.begin(u8g2, U8LOG_WIDTH1, U8LOG_HEIGHT1, u8log_buffer1);  // connect to u8g2, assign buffer
+  } 
+  else if (fontSize == 2) {
+    u8g2log.print("Font size set to 1");
+    u8g2.setFont(u8g2_font_profont12_tf); // choose a suitable font
+    u8g2log.begin(u8g2, U8LOG_WIDTH2, U8LOG_HEIGHT2, u8log_buffer2);  // connect to u8g2, assign buffer
+  }
+  else if (fontSize == 3) {
+    u8g2log.print("Font size set to 1");
+    u8g2.setFont(u8g2_font_profont15_tf);
+    u8g2log.begin(u8g2, U8LOG_WIDTH3, U8LOG_HEIGHT3, u8log_buffer3);  // connect to u8g2, assign buffer
+  }
   u8g2log.setLineHeightOffset(0); // set extra space between lines in pixel, this can be negative
   u8g2log.setRedrawMode(0);   // 0: Update screen with newline, 1: Update screen for every char  
   passedTime = millis();
@@ -43,8 +69,23 @@ void loop(void) {
     // Print string on the U8g2log window
     // Print a new line, scroll the text window content if required
     // Refresh the screen
-    u8g2log.print(incomingString);
-    u8g2log.print("\n");
+    if (incomingString == "TEST") {
+      u8g2log.print("Bluetooth disconnected... reconnecting\n");
+      Serial.write("A");
+    }
+    else if (incomingString == "FONT1") {
+      changeFont(1);
+    }
+    else if (incomingString == "FONT2") {
+      changeFont(2);
+    }
+    else if (incomingString == "FONT3") {
+      changeFont(3);
+    }
+    else {
+      u8g2log.print(incomingString);
+      u8g2log.print("\n");
+    }
     passedTime = millis();
   }
 
@@ -56,8 +97,29 @@ void loop(void) {
     // Refresh the screen
     Serial.print(incomingString);
     Serial.println();
-    u8g2log.print(incomingString);
-    u8g2log.print("\n");
+    if (incomingString == "TEST") {
+      u8g2log.print("Bluetooth disconnected... reconnecting\n");
+      MyBlue.write("A");
+      Serial.write("A");
+    }
+    else if (incomingString == "FONT1") {
+      changeFont(1);
+      MyBlue.write("A");
+    }
+    else if (incomingString == "FONT2") {
+      changeFont(2);
+      MyBlue.write("A");
+    }
+    else if (incomingString == "FONT3") {
+      changeFont(3);
+      MyBlue.write("A");
+    }
+    else {
+      u8g2log.print(incomingString);
+      u8g2log.print("\n");
+      MyBlue.write("A");
+      Serial.write("A");
+    }
     passedTime = millis();
   }
       
